ab0e47dbc4bed05887268a9d2b225014c8dcb119
Luis Jover <luisjoverdev@gmail.com>
2023-09-13 17:32:57 +0200

    fix(playlists controllers): fix bug creating playlist

--- src/routes/playlists.routes.ts
+++ src/routes/playlists.routes.ts
@@ -4,7 +4,7 @@ import { createPlayList, deletePlayList, getAllPlayLists, getPlayList, updatePla
 const playListsRouter: Router = Router();
 
 playListsRouter
-    .post("/userId", createPlayList)
+    .post("/:userId", createPlayList)
     .get("/", getAllPlayLists)
     .get("/:playListId", getPlayList)
     .put("/:playListId", updatePlayList)
