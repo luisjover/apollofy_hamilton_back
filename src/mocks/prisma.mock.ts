5dbba0c299b62941c2d93aa9d1c9d503fbb8709c
Ali Hourag <ali.hourag@gmail.com>
2023-10-16 11:57:51 +0200

    fix(prisma exportation)

--- src/mocks/prisma.mock.ts
+++ src/mocks/prisma.mock.ts
@@ -1,15 +1,15 @@
-import Prisma from "@prisma/client";
-import { mockDeep, mockReset } from "jest-mock-extended";
+import Prisma, { PrismaClient } from "@prisma/client";
+import { DeepMockProxy, mockDeep, mockReset } from "jest-mock-extended";
 
 
 import prismaClient from "../db/clientPrisma";
 
-jest.mock("../db/clientPrisma", () => ({
+jest.mock("../db/clientPrisma.ts", () => ({
     __esModule: true, default: mockDeep<Prisma.PrismaClient>()
 }));
 
 beforeEach(() => {
     mockReset(prismaMock);
 });
-
-export const prismaMock = prismaClient;
+export const prismaMock = Prisma as unknown as DeepMockProxy<PrismaClient>
+// export const prismaMock = prismaClient;
